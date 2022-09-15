02fcea7a86f7686657559a99ccd54b66cc2aee98
muNiao620 <434976939@qq.com>
2022-09-15 15:37:44 +0800

    搭建静态结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,12 @@
+import {Card} from 'antd'
+import logo from '../../assets/logo.png'
+import './index.scss'
 function Login(){
   return (
-    <div>
-      Login
+    <div className='login'>
+      <Card className='login-container'>
+        <img className='login-logo' src={logo} alt=''/>
+      </Card>
     </div>
   )
 }
