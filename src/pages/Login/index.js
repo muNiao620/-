4471da75c7544b6a1594b5fb959cb15815f54367
muNiao620 <434976939@qq.com>
2022-09-15 16:01:49 +0800

    搭建表单Form结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import {Card} from 'antd'
+import {Card ,Form, Input, Button, Checkbox} from 'antd'
 import logo from '../../assets/logo.png'
 import './index.scss'
 function Login(){
@@ -6,6 +6,24 @@ function Login(){
     <div className='login'>
       <Card className='login-container'>
         <img className='login-logo' src={logo} alt=''/>
+        <Form initialValues={{ remember: true }}>
+          <Form.Item>
+            <Input size='large' placeholder='请输入手机号' />
+          </Form.Item>
+          <Form.Item>
+            <Input size='large' placeholder='请输入验证码' />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className='login-checkbox-label'>
+              我已阅读并同意[用户协议]和[隐私条款]
+            </Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary' htmlType='submit' size='large' block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
       </Card>
     </div>
   )
