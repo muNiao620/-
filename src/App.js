78eb9681dc7fc99ff0a848fa012de03a79f32394
muNiao620 <434976939@qq.com>
2022-09-16 15:52:38 +0800

    路由鉴权，判断本地token是否存在，路由到不同页面

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter , Routes , Route  } from 'react-router-dom'
 import Login from './pages/Login';
 import Layout from './pages/Layout';
+import { AuthComponent } from './components/AuthComponent'
 function App() {
   return (
     //路由配置
@@ -8,7 +9,13 @@ function App() {
       <div className="App">
         <Routes>
           {/* 创建路由path和组件对应关系 */}
-          <Route path='/' element={<Layout />}></Route>
+          {/* Layout需要鉴权处理的 */}
+          {/* 这里的Layout不一定不能写死 要根据是否登录进行判断 */}
+          <Route path='/' element={
+            <AuthComponent>
+              <Layout />
+            </AuthComponent>
+          }></Route>
           <Route path='/Login' element={<Login />}></Route>
         </Routes>
       </div>
