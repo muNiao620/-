e06a73b3e44093bc46061c39f3764fc8fdb4aba6
muNiao620 <434976939@qq.com>
2022-09-17 15:00:20 +0800

    筛选区域结构搭建

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,8 +1,66 @@
+import { Link } from 'react-router-dom'
+import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select } from 'antd'
+import 'moment/locale/zh-cn'
+import locale from 'antd/es/date-picker/locale/zh_CN'
+import './index.scss'
+import { values } from 'mobx'
 
-const Article = () =>{
+const { Option } = Select
+const { RangePicker } = DatePicker
+
+const Article = () => {
+  const onFinish = (values)=>{
+    console.log(values)
+  }
   return (
     <div>
-      Article
+      <Card
+        title={
+          <Breadcrumb separator=">">
+            <Breadcrumb.Item>
+              <Link to="/home">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>内容管理</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+        style={{ marginBottom: 20 }}
+      >
+        <Form 
+          onFinish={onFinish}
+          initialValues={{ status: null }}>
+          <Form.Item label="状态" name="status">
+            <Radio.Group>
+              <Radio value={null}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>审核通过</Radio>
+              <Radio value={3}>审核失败</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="频道" name="channel_id">
+            <Select
+              placeholder="请选择文章频道"
+              defaultValue="lucy"
+              style={{ width: 120 }}
+            >
+              <Option value="jack">Jack</Option>
+              <Option value="lucy">Lucy</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="日期" name="date">
+            {/* 传入locale属性 控制中文显示*/}
+            <RangePicker locale={locale}></RangePicker>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ marginLeft: 80 }}>
+              筛选
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
